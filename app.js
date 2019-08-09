a0c8e2e7e7db95c5a15bd5e5b61fd549e081186a
Lebubblez <leda.erlandson@gmail.com>
2019-08-09 11:31:59 -1000

    added the api functionality in calling the temperature, description, and timezone location

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 window.addEventListener('load', ()=> {
     let long;
     let lat;
+    let temperatureDesciption = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const api = `https://api.darksky.net/forecast/c188b52dcd209b65aac2bb2316eb4401/${lat},${long}`;
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/c188b52dcd209b65aac2bb2316eb4401/${lat},${long}`;
 
             fetch(api)
             .then(response => {
                 return response.json();
             })
             .then(data => {
-                console.log(data);
+                const {temperature, summary} = data.currently;
+                //set dom elements from the api
+                temperatureDegree.textContent = temperature;
+                temperatureDesciption.textContent = summary;
+                locationTimezone.textContent = data.timezone;
             });
         });
 
