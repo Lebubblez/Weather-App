838bb039ed6160fbfc4bd39ea93a35364ebe2978
Lebubblez <leda.erlandson@gmail.com>
2019-08-09 11:58:30 -1000

    added icons and weather from c/f icon

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ window.addEventListener('load', ()=> {
     let temperatureDesciption = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
     let locationTimezone = document.querySelector('.location-timezone');
+    let temperatureSection = document.querySelector('.temperature');
+    const temperatureSpan = document.querySelector('.temperature span');
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
@@ -18,14 +20,34 @@ window.addEventListener('load', ()=> {
                 return response.json();
             })
             .then(data => {
-                const {temperature, summary} = data.currently;
+                const {temperature, summary, icon} = data.currently;
                 //set dom elements from the api
                 temperatureDegree.textContent = temperature;
                 temperatureDesciption.textContent = summary;
                 locationTimezone.textContent = data.timezone;
+                //set icon
+                setIcons(icon, document.querySelector(".icon"));
+
+                //change temperature to celcius/farenheit
+                temperatureSection.addEventListener('click', () => {
+                    if(temperatureSpan.textContent === "F"){
+                        temperatureSpan.textContent = "C";
+                    } else {
+                        temperatureSpan.textContent = "F";
+                    }
+                });
+
             });
         });
 
         
+    }
+
+    function setIcons(icon, iconID){
+        const skycons = new Skycons({color: "white"});
+        //looks for every line, and replaces with an underscore & to uppercase
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
     }
 })
